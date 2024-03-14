92f6809646a66fd0629e85abe34bce4b38dbaa14
Ahmed Bazina <32731620+ahmedbazina@users.noreply.github.com>
2024-03-14 23:22:57 +0000

    added projects and restructured the folders and paths

---
+++ script.js
@@ -0,0 +1,70 @@
+// Click events for buttons
+const portfolio = document.getElementById("portfolio");
+const portfolioBtn = document.getElementById("portfolio-btn");
+const skills = document.getElementById("skills");
+const skillsBtn = document.getElementById("skills-btn");
+
+portfolioBtn.addEventListener("click", (event) => {
+  skills.style.display = "none";
+  portfolio.style.display = "flex";
+  skillsBtn.classList.remove("active-btn");
+  portfolioBtn.classList.add("active-btn");
+});
+
+skillsBtn.addEventListener("click", (event) => {
+  skills.style.display = "flex";
+  portfolio.style.display = "none";
+  portfolioBtn.classList.remove("active-btn");
+  skillsBtn.classList.add("active-btn");
+});
+
+// Light & Dark Theme
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleThemeButton = document.getElementById("toggleTheme");
+  const themeIcon = document.querySelector('img[alt="theme icon"]');
+  const siteLogo = document.querySelector('img[alt="site logo"]');
+  const githubLogo = document.querySelector('img[alt="github logo"]');
+  const linkedinLogo = document.querySelector('img[alt="linkedin logo"]');
+  const emailLogo = document.querySelector('img[alt="email logo"]');
+
+  const lightLogos = {
+    site: "assets/icons/site_light.png",
+    github: "assets/icons/github_light.png",
+    linkedin: "assets/icons/linkedin_light.png",
+    email: "assets/icons/email_light.png",
+    theme: "assets/icons/light.png",
+  };
+
+  const darkLogos = {
+    site: "assets/icons/site_dark.png",
+    github: "assets/icons/github_dark.png",
+    linkedin: "assets/icons/linkedin_dark.png",
+    email: "assets/icons/email_dark.png",
+    theme: "assets/icons/dark.png",
+  };
+
+  function setTheme(isDark) {
+    siteLogo.src = isDark ? darkLogos.site : lightLogos.site;
+    githubLogo.src = isDark ? darkLogos.github : lightLogos.github;
+    linkedinLogo.src = isDark ? darkLogos.linkedin : lightLogos.linkedin;
+    emailLogo.src = isDark ? darkLogos.email : lightLogos.email;
+    themeIcon.src = isDark ? darkLogos.theme : lightLogos.theme;
+  }
+
+  toggleThemeButton.addEventListener("click", () => {
+    const isDark = document.body.classList.toggle("dark-theme");
+    localStorage.setItem("isDark", isDark);
+
+    setTheme(isDark);
+  });
+
+  const loadTheme = () => {
+    const isDark = localStorage.getItem("isDark") === "true";
+    document.body.classList.toggle("dark-theme", isDark);
+
+    setTheme(isDark);
+  };
+
+  // Load saved theme from local storage or default to light theme
+  loadTheme();
+});
